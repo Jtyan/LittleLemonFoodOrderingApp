5696cc9a32f666c1a536fde4fadc1630aa899288
Joeltt <joelttm15@gmail.com>
2025-10-16 16:02:13 +0100

    feat: implement custom fonts and HeaderLogo on the header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,32 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+import HeaderLogo from "../component/headerLogo";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded, error] = useFonts({
+    "Karla-Regular": require("../assets/fonts/Karla-Regular.ttf"),
+    "MarkaziText-Regular": require("../assets/fonts/MarkaziText-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
+  return (
+    <Stack
+      screenOptions={{
+        headerTitle: () => <HeaderLogo />,
+        headerTitleAlign: "center",
+      }}
+    />
+  );
 }
