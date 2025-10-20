0f2bfbda4527a49afce908382cc6fcd097d1c7c7
Joeltt <joelttm15@gmail.com>
2025-10-20 16:49:48 +0100

    asset: add Karla and MarkaziText typefaces

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,7 +7,16 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [fontsLoaded, error] = useFonts({
+    "Karla-ExtraLight": require("../assets/fonts/Karla-ExtraLight.ttf"),
+    "Karla-Light": require("../assets/fonts/Karla-Light.ttf"),
     "Karla-Regular": require("../assets/fonts/Karla-Regular.ttf"),
+    "Karla-Medium": require("../assets/fonts/Karla-Medium.ttf"),
+    "Karla-SemiBold": require("../assets/fonts/Karla-SemiBold.ttf"),
+    "Karla-Bold": require("../assets/fonts/Karla-Bold.ttf"),
+    "Karla-ExtraBold": require("../assets/fonts/Karla-ExtraBold.ttf"),
+    "MarkaziText-Bold": require("../assets/fonts/MarkaziText-Bold.ttf"),
+    "MarkaziText-SemiBold": require("../assets/fonts/MarkaziText-SemiBold.ttf"),
+    "MarkaziText-Medium": require("../assets/fonts/MarkaziText-Medium.ttf"),
     "MarkaziText-Regular": require("../assets/fonts/MarkaziText-Regular.ttf"),
   });
 
