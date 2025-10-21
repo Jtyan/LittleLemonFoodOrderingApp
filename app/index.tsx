b4d3a103dbb2733f56d8d37bc4e93c25bbcf6da3
Joeltt <joelttm15@gmail.com>
2025-10-21 14:54:11 +0100

    feat: implement redirect logic on app index which redirects to home if profile exist, else to onboarding.

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,40 @@
-import Onboarding from "./onboarding"
+import useGetUserProfile from "@/hooks/useGetUserProfile";
+import { router } from "expo-router";
+import { useEffect } from "react";
+import { ActivityIndicator, Pressable, Text, View } from "react-native";
 
 const Index = () => {
-  return (
-    <Onboarding/>
-  )
-}
+  const { isLoading, isError, profile } = useGetUserProfile();
 
-export default Index
+  useEffect(() => {
+    if (isLoading || isError) return
+    if (profile) {
+      router.replace("/home");
+    } else {
+      router.replace("/onboarding");
+    }
+  }, [isLoading, isError, profile]);
+
+  console.log(isLoading, isError, profile);
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  if (isError) {
+    return (
+      <View>
+        <Text>Oops, something went wrong!</Text>
+        <Pressable onPress={() => router.replace("/")}>
+          <Text>Refresh</Text>
+        </Pressable>
+      </View>
+    );
+  }
+  return null;
+};
+
+export default Index;
