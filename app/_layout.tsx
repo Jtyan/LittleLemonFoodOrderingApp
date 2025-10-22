62ad8cb74fa470106df039d10dce84da28338c40
Joeltt <joelttm15@gmail.com>
2025-10-22 18:09:24 +0100

    chore: rename import and change Logo icon

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,7 @@
 import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
-import HeaderLogo from "../component/headerLogo";
+import HeaderLogo from "../component/HeaderLogo";
 
 SplashScreen.preventAutoHideAsync();
 
