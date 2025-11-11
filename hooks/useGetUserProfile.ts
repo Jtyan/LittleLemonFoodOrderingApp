87a97e6bc9e8e90994250448c0cf6cf01ec07d67
Joeltt <joelttm15@gmail.com>
2025-11-11 12:10:03 +0000

    feat: get user profile from supabase auth instead of AsyncStorage

--- hooks/useGetUserProfile.ts
+++ hooks/useGetUserProfile.ts
@@ -1,36 +1,31 @@
+import { getUserProfile } from "@/lib/auth";
 import { UserProfile } from "@/types/userProfile";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useCallback, useEffect, useState } from "react";
 
-
 const useGetUserProfile = () => {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const [profile, setProfile] = useState<UserProfile | null>(null);
 
-  const refetch = useCallback(async() => {
-    setIsLoading(true)
-    setIsError(false)
-    try{
-        const profile = await AsyncStorage.getItem("userProfile");
-        if(profile) {
-        setProfile(JSON.parse(profile) as UserProfile);
-        } else {
-            setProfile(null)
-        }
-      } catch (e) {
-        console.error("Hook: Unable to get User Profile", e);
-        setIsError(true);
-      } finally {
-        setIsLoading(false);
-      }
-    }, []);
+  const refetch = useCallback(async () => {
+    setIsLoading(true);
+    setIsError(false);
+    try {
+      const userProfile = await getUserProfile();
+      setProfile(userProfile);
+    } catch (e) {
+      console.error("Hook: Unable to get User Profile", e);
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
 
   useEffect(() => {
-    refetch()
+    refetch();
   }, [refetch]);
 
-  return { isLoading, isError, profile, refetch};
+  return { isLoading, isError, profile, refetch };
 };
 
 export default useGetUserProfile;
