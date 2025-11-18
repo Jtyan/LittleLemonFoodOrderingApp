aba05a91240dedc3584d1c972a7eb9324cd332dc
Joeltt <joelttm15@gmail.com>
2025-11-18 14:07:14 +0000

    feat: use dotenv file to store api keys and supabase url

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = "https://ftnwyltwkujcwhrurncs.supabase.co"
-const supabasePublishableKey = "sb_publishable_tKvafKclNVSkB5FvmWrRRA_VYwKyTqs"
+
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!
+const supabasePublishableKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!
 export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
   auth: {
     storage: AsyncStorage,
